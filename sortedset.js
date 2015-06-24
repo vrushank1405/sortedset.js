36aa7bf89ba218848f46a78742ad51ec3cf7b2b9
Andrew Jo <andrewjo@gmail.com>
2015-06-24 13:12:13 -0700

    Initial skeleton code

---
+++ sortedset.js
@@ -0,0 +1,56 @@
+var sortedset = (function() {
+  "use strict";
+
+  // Internal private array which holds actual set elements
+  this.setArray = [];
+
+  return {
+    /* Returns true if a given item exists in the set
+     */
+    contains: function() {
+      // TODO: Implement contains method
+    },
+
+    /* Gets elements between startIndex and endIndex. If endIndex is omitted, a
+     * single element at startIndex is returned.
+     */
+    getItems: function(startIndex, endIndex) {
+      // TODO: Implement getItems method
+    },
+
+    /* Gets all items between specified value range.
+     */
+    getItemsBetween: function(lbound, ubound) {
+      // TODO: Implement getItemsBetween method
+    },
+
+    /* Adds new element to the set if not already in set
+     */
+    add: function(element) {
+      // TODO: Implement add method
+    },
+
+    remove: function(element) {
+      // TODO: Implement remove method
+    },
+
+    /* Removes element at index location
+     */
+    removeAt: function(index) {
+      // TODO: Implement removeAt method
+    },
+
+    /* Removes elements that are larger than lower bound and smaller than upper
+     * bound
+     */
+    removeBetween: function(lbound, ubound) {
+     // TODO: Implement removeBetween method
+    }
+
+    /* Removes all elements from the set
+     */
+    clear: function() {
+      // TODO: Implement clear method
+    }
+  };
+});
