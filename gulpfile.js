40208256f0503f2c5d45e602f2bb0a5d52400974
Andrew Jo <andrewjo@gmail.com>
2015-06-24 18:19:54 -0700

    Run mocha tests in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 
 gulp.task('lint', function() {
   return gulp.src('./sortedset.js')
              .pipe(jshint())
-             .pipe(jshint.reporter('default'));
+             .pipe(jshint.reporter('default'))
+             .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', ['lint'], function() {
+  return gulp.src('test/*.js')
+             .pipe(mocha());
 });
