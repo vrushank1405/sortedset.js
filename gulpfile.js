6e0ae8db225bbf03072cdcc14a35b61e8006c606
Andrew Jo <andrewjo@gmail.com>
2015-07-20 12:44:18 -0700

    Add JSHint configs

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var config = require('./package');
+var jshintConfig = config.jshintConfig;
 
 gulp.task('lint', function() {
   return gulp.src('./sortedset.js')
-             .pipe(jshint())
-             .pipe(jshint.reporter('default'))
-             .pipe(jshint.reporter('fail'));
+             .pipe(jshint(jshintConfig))
+             .pipe(jshint.reporter('default'));
 });
 
 gulp.task('test', ['lint'], function() {
